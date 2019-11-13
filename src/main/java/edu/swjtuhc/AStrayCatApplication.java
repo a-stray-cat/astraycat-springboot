490f524dc8de6a4fb7db208f5e9aafaec0ce9bae
a-stray-cat <1191876950@qq.com>
2019-11-13 13:20:25 +0800

    create a new project

---
+++ src/main/java/edu/swjtuhc/AStrayCatApplication.java
@@ -0,0 +1,13 @@
+package edu.swjtuhc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AStrayCatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AStrayCatApplication.class, args);
+	}
+
+}
